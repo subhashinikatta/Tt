4f4a376c05d815b5f8db1ab24b86a10b62123987
subhashinikatta <kshubhashini-scholar@hcl.com>
2017-04-19 17:15:25 +0530

    updated

---
+++ Register/src/web/Constants.java
@@ -0,0 +1,9 @@
+package web;
+
+public class Constants {
+    public static String dbClass = "com.mysql.jdbc.Driver";
+    private static String dbName= "userdb";
+    public static String dbUrl = "jdbc:mysql://localhost:3306/"+dbName;
+    public static String dbUser = "subbu";
+    public static String dbPwd = "subbu123";
+}
